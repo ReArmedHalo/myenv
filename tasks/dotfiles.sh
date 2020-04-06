5a068991b1360fd30750823fa1c2c5fdbdb40cc6
Dustin Schreiber <dustin@schreiber.us>
2020-04-06 11:49:00 -0400

    Double quotes matter!

--- tasks/dotfiles.sh
+++ tasks/dotfiles.sh
@@ -3,11 +3,11 @@
 runTask() {
     installPackage git
     if [ -d ~/dotfiles ]; then
-       git -C '$HOME/dotfiles' pull
+       git -C "$HOME/dotfiles" pull
     else
-       git clone https://github.com/ReArmedHalo/dotfiles.git '$HOME/dotfiles'
+       git clone https://github.com/ReArmedHalo/dotfiles.git "$HOME/dotfiles"
         if [ $? = 0 ]; then
-           ln -sf '$HOME/dotfile/.zshrc' '$HOME/.zshrc'
+           ln -sf "$HOME/dotfile/.zshrc" "$HOME/.zshrc"
         fi
     fi
 }
