865d5957625daff232221dcaba27335b969b359a
Dustin Schreiber <dustin@schreiber.us>
2020-04-06 12:32:56 -0400

    ZSH and OMZ install fix

--- tasks/zsh.sh
+++ tasks/zsh.sh
@@ -3,6 +3,8 @@
 runTask() {
     installPackage zsh
     
-    sh -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended'
+    $(which sh) -c '$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh) --unattended'
+    printf '%sTime to set the default shell to ZSH!%s\n' "$tty_boldtty_green" "$tty_reset$tty_white"
     chsh -s $(which zsh)
+    printf '%s' "$tty_reset"
 }
