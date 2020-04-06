dd4fc53f76da0c27ecad79c7e3014abb9bd66eda
Dustin Schreiber <dustin@schreiber.us>
2020-04-06 10:05:07 -0400

    This is stupid already... There has to be a better way

--- tasks/dotfiles.sh
+++ tasks/dotfiles.sh
@@ -3,13 +3,11 @@
 runTask() {
     installPackage git
     if [ -d ~/dotfiles ]; then
-        git -C "~/dotfiles" pull
+        su $MYSELF -c "git -C '~/dotfiles' pull"
     else
-        git clone https://github.com/ReArmedHalo/dotfiles.git "~/dotfiles"
+        su $MYSELF -c "git clone https://github.com/ReArmedHalo/dotfiles.git '~/dotfiles'"
         if [ $? = 0 ]; then
-            ln -sf "~/dotfile/.zshrc" "~/.zshrc"
+            su $MYSELF -c "ln -sf '~/dotfile/.zshrc' '~/.zshrc'"
         fi
-        chown $MYSELF: -R "~/dotfiles"
-        chown $MYSELF: "~/.zshrc"
     fi
 }
