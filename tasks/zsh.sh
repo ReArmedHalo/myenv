567efc98d5f7754f1fdab5729219ee245f8da319
Dustin Schreiber <dustin@schreiber.us>
2020-05-15 21:39:15 -0400

    Update zsh.sh
    Testing fix for #6

--- tasks/zsh.sh
+++ tasks/zsh.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 runTask() {
-    installPackage zsh
+    if [ ! "$OS_NAME" = "macOS" ]; then
+        installPackage zsh
+    fi
     
     $(which sh) -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
     printf '%sTime to set the default shell to ZSH!%s\n' "$tty_boldtty_green" "$tty_reset$tty_white"
-    chsh -s $(which zsh)
+    chsh -s $(grep '^/.*/zsh$' /etc/shells | tail -1)
     printf '%s' "$tty_reset"
     if [ -d ~/dotfiles ]; then
         rm -f ~/.zshrc
